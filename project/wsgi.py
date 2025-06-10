f0e2945a63a331c78f0909d3b2e55529ed26bb95
pniyitanga250 <peacemakerproofficial@gmail.com>
2025-06-11 00:35:23 +0200

    1

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,10 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
 app=application
